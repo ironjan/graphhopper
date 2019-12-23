00f9152e0deaaa5f4b9624c4aecaa080ab3026bd
Jan Lippert <lippertsjan@gmail.com>
2019-12-23 13:11:33 +0100

    Removed vehicle from routing call

--- node2node_routing/src/main/java/LatLonRouting.java
+++ node2node_routing/src/main/java/LatLonRouting.java
@@ -14,8 +14,6 @@ public class LatLonRouting extends RoutingExample {
     public PathWrapper getRoute(double fromLat, double fromLon, double toLat, double toLon, double fromLvl, double toLvl) {
 // simple configuration of the request object, see the GraphHopperServlet classs for more possibilities.
         GHRequest req = new GHLevelRequest(fromLat, fromLon, toLat, toLon, Double.NaN, Double.NaN, fromLvl, toLvl)
-//                .setWeighting("shortest")
-                .setVehicle("foot")
                 .setLocale(Locale.GERMAN);
         String weighting = req.getWeighting();
         GHResponse rsp = hopper.route(req);
