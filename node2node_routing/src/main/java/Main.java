eee48be00a8467dcb16144728b9836faf532d6b5
Jan Lippert <lippertsjan@gmail.com>
2019-12-23 12:11:09 +0100

    Refactoring and more test cases

--- node2node_routing/src/main/java/Main.java
+++ node2node_routing/src/main/java/Main.java
@@ -5,23 +5,38 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
     private static Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    private final GraphHopper hopper;
+
+    public Main(GraphHopper hopper) {
+        this.hopper = hopper;
+    }
 
     public static void main(String[] args) {
         String osmFile = args[0];
         String graphFolder = args[1];
         DirectoryDeleter.deleteDirectory(graphFolder);
 
-
         GraphHopper hopper = GraphLoader.get(osmFile, graphFolder);
-        new Main();
+        new Main(hopper).runLatLonTest();
 
-        PathWrapper latLonRoute = new LatLonRouting(hopper)
-                .getRoute(48.140375, 11.559291, 48.139029, 11.5687);
-        PathPrinter.print(latLonRoute);
-
-        PathWrapper nodeIdRoute = new NodeIdRouting(hopper).getRoute(121, 77);
-        PathPrinter.print(nodeIdRoute);
     }
 
+    private void runLatLonTest() {
+        singleTest(48.1403750, 11.5592910, 48.139029, 11.568700);
+        singleTest(48.1387140, 11.5648430, 48.138832, 11.567654);
+        singleTest(48.1384420, 11.5649660, 48.139029, 11.568700);
+
+        // Start is exactly on top of a "Stachuspassage, 1. Untergeschoss" node
+        singleTest(48.1394991, 11.5659233, 48.139029, 11.568700);
+    }
+
+    private void singleTest(double fromLat, double fromLon, double toLat, double toLon) {
+        LOGGER.debug("Route from {},{} to {},{}.", fromLat, fromLon, toLat, toLon);
+
+        LatLonRouting latLonRouting = new LatLonRouting(hopper);
+
+        PathWrapper latLonRoute = latLonRouting.getRoute(fromLat, fromLon, toLat, toLon);
+        PathPrinter.print(latLonRoute);
+    }
 
 }
