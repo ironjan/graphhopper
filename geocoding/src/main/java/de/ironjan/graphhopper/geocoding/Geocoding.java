7b35f1f37758fdf86a98431642996cd5e5344a56
Jan Lippert <lippertsjan@gmail.com>
2020-01-11 21:26:15 +0100

    Added some logging

--- geocoding/src/main/java/de/ironjan/graphhopper/geocoding/Geocoding.java
+++ geocoding/src/main/java/de/ironjan/graphhopper/geocoding/Geocoding.java
@@ -1,5 +1,8 @@
 package de.ironjan.graphhopper.geocoding;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,19 +12,27 @@ import java.util.List;
 public class Geocoding {
     private final HashMap<String, Poi> knownPois = new HashMap<>();
     private List<Geocoding> otherSources = new ArrayList<>();
+    private Logger logger = LoggerFactory.getLogger(Geocoding.class);
     ;
 
     public Poi getByName(String name) {
+        logger.debug("Retrieving information about '{}'.", name);
+
         if (knownPois.containsKey(name)) {
-            return knownPois.get(name);
+            Poi poi = knownPois.get(name);
+            logger.debug("Found POI: {}.", poi);
+            return poi;
         }
 
+        logger.debug("Delegating to other sources, if any.");
         for (Geocoding src: otherSources) {
             Poi potentialResult = src.getByName(name);
             if(potentialResult != null) {
                 return potentialResult;
             }
         }
+
+        logger.debug("No POI with name {} found.", name);
 
         return null;
     }
@@ -61,14 +72,25 @@ public class Geocoding {
     }
 
     public static class Loader {
+
+        private static Logger logger = LoggerFactory.getLogger(Loader.class);
+
         public static Geocoding fromFile(String osmFile) throws IOException {
+            logger.info("Initializing Geocoding class from file {}.", osmFile);
+
             GeocodingOsmReader osmReader = new GeocodingOsmReader();
             osmReader.setOsmFile(new File(osmFile));
             List<Poi> pois = osmReader.readGraph();
+
+            logger.debug("Read file.");
+
             Geocoding geocoding = new Geocoding();
             for (Poi poi : pois) {
                 geocoding.add(poi);
             }
+
+            logger.info("Loaded geocoding data with {} pois.", geocoding.size());
+
             return geocoding;
         }
 
